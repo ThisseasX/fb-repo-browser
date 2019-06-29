4bdd37d1e0de07f8c0c4cb94dd99fec46eb73135
Thisseas X <thisseasx@yahoo.com>
2019-06-29 23:30:33 +0300

    Initial Commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import '../../../node_modules/normalize.css/normalize.css';
+import './App.css';
+import Navbar from '../Navbar/Navbar';
+import Main from '../Main/Main';
+
+export default () => (
+  <Fragment>
+    <Navbar />
+    <Main />
+  </Fragment>
+)
