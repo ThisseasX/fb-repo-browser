a4e1527dea1727342e1bde9a50c1ca2d941220a7
t.xanthopoulos <t.xanthopoulos@intralot.com>
2019-07-01 11:21:38 +0300

    Fix github auth

--- src/components/Results/ResultsBody/ResultsBody.js
+++ src/components/Results/ResultsBody/ResultsBody.js
@@ -7,12 +7,22 @@ export default () => {
   const [repos, setRepos] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.github.com/users/facebook/repos?per_page=100')
+    fetch('https://api.github.com/users/facebook/repos?per_page=100', {
+      headers: {
+        'Authorization': 'token bd0e43bee286d8c2fa8df45443e97c2e2f288857'
+      }
+    })
       .then(res => res.json())
       .then(json => {
         const responseRepos = [];
 
+        if (!Array.isArray(json)) {
+          console.log(json.message);
+          return;
+        }
+
         json.some(({ id, name, description, stargazers_count }, index) => {
+          if (index >= 8) return true;
 
           responseRepos.push({
             id,
