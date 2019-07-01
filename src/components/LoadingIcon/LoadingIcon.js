42a6ef9d4d3cc7511bc4d6a5a86b62b3c5ba39e7
Thisseas X <thisseasx@yahoo.com>
2019-07-01 19:28:59 +0300

    Refactor icons to components

--- src/components/LoadingIcon/LoadingIcon.js
+++ src/components/LoadingIcon/LoadingIcon.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { icon } from './LoadingIcon.module.sass'
 
 export default () => (
-    <svg className={icon} version="1.1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" width="80"
-        height="80">
-        <defs>
-            <linearGradient id="rStscXQFhfAK" x1="0px" x2="40px" y1="40px" y2="40px" gradientUnits="userSpaceOnUse">
-                <stop offset="0" stop-color="yellow" />
-                <stop offset="1" stop-color="#f76b1c" />
-            </linearGradient>
-        </defs>
-        <g>
-            <path fill="none" stroke="#f0f0f0" paint-order="fill stroke markers"
-                d=" M 70 40 A 30 30 0 1 1 69.99998500000125 39.97000000499998" stroke-miterlimit="10"
-                stroke-dasharray="" />
-            <path fill="none" stroke="url(#rStscXQFhfAK)" paint-order="fill stroke markers"
-                d=" M 10 39.99999999999999 A 30 30 0 1 1 40 70" stroke-linecap="round" stroke-miterlimit="10"
-                stroke-width="5" stroke-dasharray="" />
-        </g>
-    </svg>
+  <svg className={icon} version="1.1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" width="80"
+    height="80">
+    <defs>
+      <linearGradient id="rStscXQFhfAK" x1="0px" x2="40px" y1="40px" y2="40px" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="yellow" />
+        <stop offset="1" stop-color="#f76b1c" />
+      </linearGradient>
+    </defs>
+    <g>
+      <path fill="none" stroke="#f0f0f0" paint-order="fill stroke markers"
+        d=" M 70 40 A 30 30 0 1 1 69.99998500000125 39.97000000499998" stroke-miterlimit="10"
+        stroke-dasharray="" />
+      <path fill="none" stroke="url(#rStscXQFhfAK)" paint-order="fill stroke markers"
+        d=" M 10 39.99999999999999 A 30 30 0 1 1 40 70" stroke-linecap="round" stroke-miterlimit="10"
+        stroke-width="5" stroke-dasharray="" />
+    </g>
+  </svg>
 );
--- src/components/Repository/RepoStars/RepoStars.js
+++ src/components/Repository/RepoStars/RepoStars.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { wrapper, icon, count } from './RepoStars.module.sass';
+import StarIcon from '../../StarIcon/StarIcon';
 
 export default ({ stars }) => (
   <div className={wrapper}>
     <div className={icon}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
-        <path stroke="white" fill="white"
-          d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
-      </svg>
+      <StarIcon />
     </div>
     <p className={count}>{stars}</p>
   </div>
